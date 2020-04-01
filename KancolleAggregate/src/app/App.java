2171941e7e6160233b21bca8256b2dafec628bce
Er-Gray <sakuya.mybride@gmail.com>
2020-04-01 22:09:10 +0900

    一応完成形

--- KancolleAggregate/src/app/App.java
+++ KancolleAggregate/src/app/App.java
@@ -11,8 +11,9 @@ public class App {
         int item=0;
         int normalCount=0;
         int specialCount=0;
+        int emotion=0;
         ArrayList<String> equipment = new ArrayList<String>();
-        ResourceRead resourceRead= new ResourceRead(resource,nowResource,free,item,normalCount,specialCount,equipment);
+        ResourceRead resourceRead= new ResourceRead(resource,nowResource,free,emotion,item,normalCount,specialCount,equipment);
 
         Arrays.fill(resource,0);
         Arrays.fill(nowResource,0);
@@ -20,9 +21,73 @@ public class App {
         String[] aircraft=readCSV("Aircraft.csv");
         String[] special=readCSV("Special.csv");
         resourceRead.resourceInit();
+        Scanner scanner=new Scanner(System.in);
         while(true){
             System.out.println("出た資材に応じて値を入力してEnter");
-            System.out.println("資材の確認:0\n燃料追加:1\n弾薬追加:2\n鋼材追加:3\nボーキ追加:4\n任意追加:5\n感情追加:6\n特殊戦果表:7");
+            System.out.println("資材の確認:0\n燃料追加:1\n弾薬追加:2\n鋼材追加:3\nボーキ追加:4\n任意追加:5\n感情追加:6\n特殊戦果表:7\n結果をクリップボードにコピー:8");
+            try{
+                int num=scanner.nextInt();
+                switch(num){
+                    case 0:
+                        resourceRead.printResource();
+                        break;
+                    case 1:
+                    case 2:
+                    case 3:
+                    case 4:
+                        resourceRead.addResource(num);
+                        resourceRead.setNormalCount(++normalCount);
+                        break;
+                    case 5:
+                        resourceRead.addFree();
+                        resourceRead.setNormalCount(++normalCount);
+                        break;
+                    case 6:
+                        resourceRead.setEmotion(++emotion);
+                        resourceRead.setNormalCount(++normalCount);
+                        break;
+                    case 7:
+                        System.out.println("特殊戦果表\nすべての資材+3:1\nアイテム:2\n家具コイン:3\n砲類開発表:4\n艦載機開発表:5\n新特殊開発表:6");
+                        int spNum=scanner.nextInt();
+                        switch(spNum){
+                            case 1:
+                                resourceRead.all3Resource();
+                                resourceRead.setSpecialCount(++specialCount);
+                                break;
+                            case 2:
+                                resourceRead.setItem(++item);
+                                resourceRead.setSpecialCount(++specialCount);
+                                break;
+                            case 3:
+                                resourceRead.setCoin(++resource[4],++nowResource[4]);
+                                resourceRead.setSpecialCount(++specialCount);
+                                break;
+                            case 4:
+                                resourceRead.developmentTable(guns);
+                                resourceRead.setSpecialCount(++specialCount);
+                                break;
+                            case 5:
+                                resourceRead.developmentTable(aircraft);
+                                resourceRead.setSpecialCount(++specialCount);
+                                break;
+                            case 6:
+                                resourceRead.developmentTable(special);
+                                resourceRead.setSpecialCount(++specialCount);
+                                break;
+                            default:
+                                System.out.println("入力された値が違います");
+                        }
+                        break;
+                    case 8:
+                        resourceRead.copyToClipboard();
+                        break;
+                    default:
+                        System.out.println("入力された値が違います");
+                }
+            }catch(Exception e){
+                System.out.println("入力された値が違います");
+                e.printStackTrace();
+            }
         }
     }
 
--- KancolleAggregate/src/app/ResourceRead.java
+++ KancolleAggregate/src/app/ResourceRead.java
@@ -1,19 +1,23 @@
 package app;
 
 import java.util.*;
+import java.awt.datatransfer.*;
+import java.awt.*;
 
 class ResourceRead{
     private int[] resource=new int[5];//燃料/弾薬/鋼材/ボーキ/家具コインの順番の配列
     private int[] nowResource=new int[5];
     private ArrayList<Integer> free = new ArrayList<Integer>();
-    private int item=0;
-    private int normalCount=0;
-    private int specialCount=0;
+    private int emotion;
+    private int item;
+    private int normalCount;
+    private int specialCount;
     private ArrayList<String> equipment = new ArrayList<String>();
-    ResourceRead(int[] resource,int[] nowResource,ArrayList<Integer> free,int item,int normalCount,int specialCount,ArrayList<String> equipment){
+    ResourceRead(int[] resource,int[] nowResource,ArrayList<Integer> free,int emotion,int item,int normalCount,int specialCount,ArrayList<String> equipment){
         this.resource=resource;
         this.nowResource=nowResource;
         this.free=free;
+        this.emotion=emotion;
         this.item=item;
         this.normalCount=normalCount;
         this.specialCount=specialCount;
@@ -41,5 +45,79 @@ class ResourceRead{
             }
             break;
         }
+    }
+
+    public void setEmotion(int emotion){
+        this.emotion=emotion;
+    }
+
+    public void setItem(int item){
+        this.item=item;
+    }
+
+    public void setNormalCount(int normalCount){
+        this.normalCount=normalCount;
+    }
+
+    public void setSpecialCount(int specialCount){
+        this.specialCount=specialCount;
+    }
+
+    public void setCoin(int coin,int nowCoin){
+        this.resource[4]=coin;
+        this.nowResource[4]=nowCoin;
+    }
+
+    public void addResource(int num){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("出た量を入力してEnter");
+        int addResource=scanner.nextInt();
+        resource[num-1]+=addResource;
+        nowResource[num-1]+=addResource;
+    }
+
+    public void all3Resource(){
+        for(int i=0;i<4;i++){
+            resource[i]+=3;
+            nowResource[i]+=3;
+        }
+    }
+
+    public void addFree(){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("出た量を入力してEnter");
+        int addFree=scanner.nextInt();
+        free.add(addFree);
+    }
+
+    public void developmentTable(String[] table){
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("開発表の出目は？");
+        int developmentNum=scanner.nextInt();
+        switch(table.length){
+            case 21:
+                equipment.add(table[developmentNum-4]);
+                System.out.println(table[developmentNum-4]+"が追加されました");
+                break;
+            case 11:
+                equipment.add(table[developmentNum-2]);
+                System.out.println(table[developmentNum-2]+"が追加されました");
+                break;
+        }
+    }
+
+    public void printResource(){
+        System.out.println("獲得した資材:"+resource[0]+"/"+resource[1]+"/"+resource[2]+"/"+resource[3]+" 任意:"+Arrays.asList(free)+" 感情:"+emotion+" 家具コイン:"+resource[4]);
+        System.out.println("現在資材:"+nowResource[0]+"/"+nowResource[1]+"/"+nowResource[2]+"/"+nowResource[3]+" コイン:"+nowResource[4]);
+        System.out.println("アイテム:"+item);
+        System.out.println("出た装備:"+Arrays.asList(equipment));
+        System.out.println("通常:"+normalCount+"回 特殊:"+specialCount+"回");
+    }
+
+    public void copyToClipboard(){
+        Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        StringSelection selection = new StringSelection("獲得した資材:"+resource[0]+"/"+resource[1]+"/"+resource[2]+"/"+resource[3]+" 任意:"+Arrays.asList(free)+" 感情:"+emotion+" 家具コイン:"+resource[4]+"\n現在資材:"+nowResource[0]+"/"+nowResource[1]+"/"+nowResource[2]+"/"+nowResource[3]+" コイン:"+nowResource[4]+"\nアイテム:"+item+"\n出た装備:"+Arrays.asList(equipment)+"\n通常:"+normalCount+"回 特殊:"+specialCount+"回");
+        clipboard.setContents(selection, selection);
+        System.out.println("クリップボードにコピーしました");
     }
 }
